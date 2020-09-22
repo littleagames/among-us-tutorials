0c67b84ad43ef8a2f0bbc1fea841637391c579ca
Little a Games <developer@littleagames.com>
2020-09-21 21:25:08 -0500

    Adding UI for simon task, and initial unity project scripts

---
+++ Puzzles/Assets/Scripts/SimonTask.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimonTask : MonoBehaviour
+{
+    public List<GameObject> _simonDisplay;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ButtonPressed(int index)
+    {
+        Debug.Log($"Button Presssed: {index}");
+    }
+}
